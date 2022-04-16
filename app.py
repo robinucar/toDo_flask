e520e4a8077d202df7294ccb30c022eccd8a554a
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-16 16:30:39 +0100

    Todo Model

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # create server
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # relative path sql name
 db = SQLAlchemy(app) # initialize database
+
+
+# Todo model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Clumn(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' %self.id 
+
 
 @app.route('/') # main route
 def index():
