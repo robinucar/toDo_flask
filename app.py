150c05d89b195c996a18bb47f39a62d88ca9863f
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-16 16:23:42 +0100

    initialize database

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # create server
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # relative path sql name
+db = SQLAlchemy(app) # initialize database
 
 @app.route('/') # main route
 def index():
