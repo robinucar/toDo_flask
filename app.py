395e4ce5b0be0ac928214086aef2a488c42eb8e1
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-16 14:29:20 +0100

    seeing html template on the browser

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # create server
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/') # main route
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
